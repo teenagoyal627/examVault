b60cf84291520a9c7cac06ec3bbfb5dbcc71b7f4
teenagoyal627 <teenagoyal627@gmail.com>
2025-02-08 23:56:15 +0530

    small changes

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,6 @@ require("dotenv").config()
 
 const connectDB=async()=>{
     try{
-        // const MONGO_URI="mongodb+srv://ExamVaultProject:examVaultPassword@examvault.rotik.mongodb.net/ExamVault?retryWrites=true&w=majority&appName=ExamVault"
-        // await mongoose.connect(MONGO_URI)
         await mongoose.connect(process.env.MONGO_URI);
         console.log("mongodb is connected")
     }catch(error){
