b88624fe6797b7cbe4366bcb027569eebba7ba25
teenagoyal627 <teenagoyal627@gmail.com>
2025-08-07 15:52:58 +0530

    Remove the Footer from Registration Page

--- client/exam-vault/src/component/FrontPage/FrontPage.js
+++ client/exam-vault/src/component/FrontPage/FrontPage.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import FrontPageNavbar from '../Navbar/FrontPageNavbar'
 import FrontPageCard from './FrontPageCard'
-import Footer from '../Footer/Footer'
 
 const FrontPage = () => {
   return (
     <div>
     <FrontPageNavbar/>
      <FrontPageCard/>
-     {/* <Footer/> */}
     </div>
   )
 }
